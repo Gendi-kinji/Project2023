1dc6c3919fc897dc464c4339138d73843c4a25a0
Gendi-kinji <130544395+Gendi-kinji@users.noreply.github.com>
2023-04-23 19:24:04 +0300

    Update OOP II.java

--- OOP II.java
+++ OOP II.java
@@ -82,4 +82,61 @@ public class Calculator extends Notification{
         return FeeBal;
     }
 }
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+public class Fees extends Calculator{
+    private JFrame Frame;
+    private JLabel lblFee;
+    private JLabel lblAdministration;
+    private JLabel lblMedical;
+    private JLabel lblRemedial;
+    private JLabel lblDamages;
+    private JButton btnCancel;
+    private JButton btnOk;
+    public Fees(){
+        Frame=new JFrame("Fee notification");
+        lblFee=new JLabel("Total fee balance is"+getFeeBal());
+        lblAdministration=new JLabel("Administration fee:"+Administration);
+        lblMedical=new JLabel("Medical:"+Medical);
+        lblRemedial=new JLabel("Remedial:"+Remedial);
+        lblDamages=new JLabel("Damages"+Damages);
+        btnCancel=new JButton("Cancel");
+        btnOk=new JButton("Ok");
+        Frame.setLayout(new CardLayout());
+        Frame.add(Frame);
+        Frame.add(lblFee);
+        Frame.add(lblAdministration);
+        Frame.add(lblMedical);
+        Frame.add(lblRemedial);
+        Frame.add(lblDamages);
+        Frame.add(btnCancel);
+        Frame.add(btnOk);
+
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (btnCancel.isSelected()){
+                    Parent pare=new Parent();
+
+                }
+
+            }
+        });
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (btnOk.isSelected()){
+                    Parent pare=new Parent();
+
+                }
+            }
+        });
+
+
+    }
+
+}
+
 
